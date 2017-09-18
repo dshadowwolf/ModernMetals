f9d252ae16cdff617c8858f113a6c9d4c49498b6
dshadowwolf <dshadowwolf@gmail.com>
2017-09-17 20:19:30 -0400

    Master 1.12.1 (#66)
    * build.gradle
    
    * More 1.11.2 work
    
    * It builds!
    
    * Update build.gradle
    
    Orespawn
    Resync
    
    * Bits I missed
    
    * update annotation
    
    * sonarqube
    
    * update build.gradle
    
    * Gah!
    
    * clean up some SonarQube issues - hope you like the fix for the unused parameters bit, Jas!
    
    * clean up some SonarQube issues. (#58)
    
    * Disable old OS1 code
    
    * Fix stuff up, remove the old OS1 interfacing code and update the OreSpawn plugin to the API of v3
    
    * Update and rename CHANGELOG.TXT to CHANGELOG.txt
    
    * Master 1.11.2 (#59)
    
    * clean up some SonarQube issues - hope you like the fix for the unused parameters bit, Jas!
    
    * Fix stuff up, remove the old OS1 interfacing code and update the OreSpawn plugin to the API of v3
    
    * Cleanups
    
    * Correct horsearmor naming
    
    * Updates, cleaning, a few bugfixes
    
    * Some bits I missed in the last round
    
    * Update ModernMetals OreSpawn integration so it works with OreSpawn 3.1.x
    
    * port to OreSpawn 3.1.x interfaces (#62)
    
    * clean up some SonarQube issues - hope you like the fix for the unused parameters bit, Jas!
    
    * Fix stuff up, remove the old OS1 interfacing code and update the OreSpawn plugin to the API of v3
    
    * Update ModernMetals OreSpawn integration so it works with OreSpawn 3.1.x
    
    * fix the issues caused by the shift of the config system to a shared base
    
    * Master 1.11.2 (#63)
    
    * clean up some SonarQube issues - hope you like the fix for the unused parameters bit, Jas!
    
    * Fix stuff up, remove the old OS1 interfacing code and update the OreSpawn plugin to the API of v3
    
    * Update ModernMetals OreSpawn integration so it works with OreSpawn 3.1.x
    
    * fix the issues caused by the shift of the config system to a shared base
    
    * Initial ALT Plugin
    
    * Remove obsolete
    
    * Whoops
    
    * Updates
    
    * Nearly forgot the version.....
    
    * add an updated version of the recipe generator tool used in BaseMetals for its recipes
    
    * file got caught that shouldn't have been
    
    * add the 600-700 recipes needed for this mod
    
    * forgot that Titanium has an alloy via Rutile
    
    * final cleanups and removal of error code, next comes actual registration of stuffs
    
    * compiles, loads, seems that issues exist with generated recipes
    
    * hope this fixes the recipe issue
    
    * fix a minor mistake in the file-naming in the gen-script and in one of the recipe-entries of the gen-script
    
    * another logic bug in the generator fixed, recipes re-genned with fix in place
    
    * forgot that we have to special-handle the pressure-plate since I've got it as 'PRESSUREPLATE' in the item name-refs but it's 'pressure_plate' in the item registry names
    
    * found a bad logic bug in the generator during the pressureplate naming fix
    
    * logic bug I found in one helper also existed in another, gah!
    
    * fix the issue with the crossbow recipes - had a condition wrong
    
    * fix the bad config name for the trapdoor coverage
    
    * more recipe fixes
    
    * somehow missed!
    
    * fix some shit that needs it
    
    * making sure that we usually have the right case for a value

--- src/main/java/com/mcmoddev/modernmetals/init/ItemGroups.java
+++ src/main/java/com/mcmoddev/modernmetals/init/ItemGroups.java
@@ -2,13 +2,14 @@ package com.mcmoddev.modernmetals.init;
 
 import com.mcmoddev.basemetals.data.MaterialNames;
 import com.mcmoddev.basemetals.init.Materials;
-import com.mcmoddev.basemetals.util.Config.Options;
+import com.mcmoddev.lib.util.ConfigBase.Options;
 import com.mcmoddev.lib.data.Names;
 import com.mcmoddev.lib.init.MMDCreativeTab;
+import com.mcmoddev.lib.util.TabContainer;
 
-import net.minecraft.block.Block;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 /**
  * This class initializes all item groups in Modern Metals.
@@ -19,12 +20,13 @@ import net.minecraft.item.Item;
 public class ItemGroups extends com.mcmoddev.lib.init.ItemGroups {
 
 	private static boolean initDone = false;
-	private static final int blocksTabId  = addTab("blocks", true );;
-	private static final int itemsTabId = addTab("items", true );
-	private static final int toolsTabId = addTab("tools", true );
-	public static final MMDCreativeTab blocksTab = getTab(blocksTabId);
-	public static final MMDCreativeTab itemsTab = getTab(itemsTabId);
-	public static final MMDCreativeTab toolsTab = getTab(toolsTabId); 
+	private static final int BLOCKS_TAB_ID  = addTab("blocks", true);
+	private static final int ITEMS_TAB_ID = addTab("items", true);
+	private static final int TOOLS_TAB_ID = addTab("tools", true);
+	private static final MMDCreativeTab blocksTab = getTab(BLOCKS_TAB_ID);
+	private static final MMDCreativeTab itemsTab = getTab(ITEMS_TAB_ID);
+	private static final MMDCreativeTab toolsTab = getTab(TOOLS_TAB_ID); 
+	public static final TabContainer myTabs = new TabContainer(blocksTab, itemsTab, toolsTab);
 
 	private ItemGroups() {
 		throw new IllegalAccessError("Not a instantiable class");
@@ -42,12 +44,12 @@ public class ItemGroups extends com.mcmoddev.lib.init.ItemGroups {
 	}
 	
 	public static void setupIcons() {
-		Item blocksTabIconItem = Item.getItemFromBlock(Options.materialEnabled(com.mcmoddev.basemetals.data.MaterialNames.STEEL)?Materials.getMaterialByName(MaterialNames.STEEL).getBlock(Names.BLOCK):(Block) Materials.getMaterialByName(MaterialNames.IRON).getBlock(Names.BLOCK));
-		Item itemsTabIconItem = Options.thingEnabled("Gear")?Materials.getMaterialByName(MaterialNames.STEEL).getItem(Names.GEAR):Items.STICK;
-		Item toolsTabIconItem = Options.thingEnabled("BasicTools")?Materials.getMaterialByName(MaterialNames.STEEL).getItem(Names.SWORD):Items.DIAMOND_SWORD;
+		Item blocksTabIconItem = Item.getItemFromBlock(Options.isMaterialEnabled(com.mcmoddev.basemetals.data.MaterialNames.STEEL) ? Materials.getMaterialByName(MaterialNames.STEEL).getBlock(Names.BLOCK) : Materials.getMaterialByName(MaterialNames.IRON).getBlock(Names.BLOCK));
+		Item itemsTabIconItem = Options.isThingEnabled("Gear") ? Materials.getMaterialByName(MaterialNames.STEEL).getItem(Names.GEAR) : Items.STICK;
+		Item toolsTabIconItem = Options.isThingEnabled("BasicTools") ? Materials.getMaterialByName(MaterialNames.STEEL).getItem(Names.SWORD) : Items.DIAMOND_SWORD;
 		
-		blocksTab.setTabIconItem( blocksTabIconItem );
-		itemsTab.setTabIconItem(itemsTabIconItem);
-		toolsTab.setTabIconItem(toolsTabIconItem);
+		blocksTab.setTabIconItem(new ItemStack(blocksTabIconItem));
+		itemsTab.setTabIconItem(new ItemStack(itemsTabIconItem));
+		toolsTab.setTabIconItem(new ItemStack(toolsTabIconItem));
 	}
 }
